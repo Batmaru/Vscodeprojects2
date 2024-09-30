dbdf1f753fbe21b3e64f294fd8559a9fa9b3101b
Batmaru <marwanrafik02@gmail.com>
2024-09-30 10:51:34 +0200

    modifiche

--- lezione27settembre/client.py
+++ lezione27settembre/client.py
@@ -1,20 +1,19 @@
 import requests, json, sys
+
 base_url = "http://127.0.0.1:8080"
 
-
 def GetDatiCittadino():
-    nome = "Mario"
-    cognome = "Garibaldi"
-    dataN = "07/11/1890"
-    codF = "dcfvfg70b34h501u"
-    datiCittadino = {"nome":nome, "cognome": cognome, "dataNascita":dataN, "codFiscale":codF}
+    nome = input("Inserisci il nome: ")
+    cognome = input("Inserisci il cognome: ")
+    dataN = input("Inserisci la data di nascita (gg/mm/aaaa): ")
+    codF = input("Inserisci il codice fiscale: ")
+    datiCittadino = {"nome": nome, "cognome": cognome, "dataNascita": dataN, "codFiscale": codF}
     return datiCittadino
 
 def GetCodicefiscale():
-    cod=input('inserisci codice fiscale: ')
-    return cod
+    cod = input('Inserisci codice fiscale: ')
+    return {"codFiscale": cod}
 
-    
 while True:
     print("\nOperazioni disponibili:")
     print("1. Inserisci cittadino")
@@ -22,7 +21,7 @@ while True:
     print("3. Modifica cittadino")
     print("4. Elimina cittadino")
     print("5. Esci")
-    
+
     try:
         sOper = int(input("Cosa vuoi fare? "))
     except ValueError:
@@ -30,41 +29,35 @@ while True:
         continue
 
     if sOper == 1:
-        print("richiesto aggiungi cittadino")
+        print("Aggiunta cittadino")
         api_url = base_url + "/add_cittadino"
         jsonDataRequest = GetDatiCittadino()
-        response = requests.post(api_url,json=jsonDataRequest)
+        response = requests.post(api_url, json=jsonDataRequest)
+        print(response.json())
 
     elif sOper == 2:
-        print("richiesta dei dati del cittadino")
+        print("Richiesta dati cittadino")
         api_url = base_url + "/read_cittadino"
         jsonDataRequest = GetCodicefiscale()
-        response = requests.post(api_url,json=jsonDataRequest)
-    elif sOper ==4:
-        print("richiesta di eliminazione del cittadino")
+        response = requests.get(api_url + "/" + jsonDataRequest['codFiscale'])
+        print(response.json())
+
+    elif sOper == 3:
+        print("Modifica cittadino")
+        api_url = base_url + "/update_cittadino"
+        jsonDataRequest = GetDatiCittadino()
+        response = requests.post(api_url, json=jsonDataRequest)
+        print(response.json())
+
+    elif sOper == 4:
+        print("Eliminazione cittadino")
         api_url = base_url + "/elimina_cittadino"
         jsonDataRequest = GetCodicefiscale()
-        response = requests.post(api_url,json=jsonDataRequest)
+        response = requests.post(api_url, json=jsonDataRequest)
+        print(response.json())
 
     elif sOper == 5:
         print("Buona giornata!")
         sys.exit()
     else:
         print("Operazione non disponibile, riprova.")
-        
-   
-
-
-
-
-
-
-api_url = base_url + "/add_cittadino"
-jsonDataRequest = GetDatiCittadino()
-response = requests.post(api_url,json=jsonDataRequest)
-#print(response.json())
-print(response.status_code)
-print(response.headers["Content-Type"])
-data1 = response.json()
-print(data1)
-
--- lezione27settembre/server.py
+++ lezione27settembre/server.py
@@ -1,71 +1,63 @@
-from flask import Flask, json, jsonify, request
+from flask import Flask, jsonify, request
+
 api = Flask(__name__)
 
-
-#mettere una lista di liste dove ogni lista è un cittadino
-
-#la chiave è il codice fiscale
-#add cittadino
-#read cittadino
-#update cittadino
-#delete cittadino
-cittadini={"dcfvfg70b34h501u": { 
-    "nome": "Mario",
-    "cognome" : "Garibaldi",
-    "dataN" : "07/11/1890"}}
+# Dizionario che memorizza i cittadini (codice fiscale come chiave)
+cittadini = {
+    "dcfvfg70b34h501u": {
+        "nome": "Mario",
+        "cognome": "Garibaldi",
+        "dataN": "07/11/1890"
+    }
+}
 
 @api.route('/add_cittadino', methods=['POST'])
-
-
 def GestisciAddCittadino():
     content_type = request.headers.get('Content-Type')
-    print("chiamata Ricevuta" + 'application_type')
-    if(content_type=='application/json'):
+    if content_type == 'application/json':
         jsonReq = request.json
         codice_fiscale = jsonReq.get('codFiscale')
-        jsonResp={"Esito":"200", "Msg": "ok"}
         if codice_fiscale in cittadini:
-            return jsonify({"Esito": "400", "Msg": "Cittadino già esistente"}), 400
+            return jsonify({"Esito": "200", "Msg": "Cittadino già esistente"}), 200
         else:
             cittadini[codice_fiscale] = jsonReq
-            return json.dumps(jsonResp)
-        
-        # if any(cittadino['codFiscale'] == codice_fiscale for cittadino in cittadini):
-        #     return jsonify({"Esito": "400", "Msg": "Cittadino già esistente"}), 400
-        # # Ecco un breve riepilogo di alcuni codici di stato HTTP comuni:
-        # # 200 OK: La richiesta è stata elaborata con successo.
-        # # 400 Bad Request: La richiesta non può essere elaborata a causa di un errore nel client (ad esempio, dati malformati).
-        # # 404 Not Found: La risorsa richiesta non è stata trovata sul server.
-        # # 500 Internal Server Error: Si è verificato un errore sul server durante l'elaborazione della richiesta.
+            return jsonify({"Esito": "200", "Msg": "Cittadino aggiunto con successo"}), 200
     else:
-        return 'Content-Type not supported!'
-    
-
+        return 'Content-Type non supportato!'
 
 @api.route('/read_cittadino/<codice_fiscale>', methods=['GET'])
-def read_cittadino():
-    codice_fiscale= request.json
+def read_cittadino(codice_fiscale):
     cittadino = cittadini.get(codice_fiscale)
     if cittadino:
-        return jsonify({"Esito" : "200", "Msg" : "cittadino trovato" + str(cittadino)}), 200
-
+        return jsonify({"Esito": "200", "Msg": "Cittadino trovato", "Dati": cittadino}), 200
     else:
-        return jsonify({"Esito": "200", "Msg": "Cittadino non trovato"}), 200
+        return jsonify({"Esito": "404", "Msg": "Cittadino non trovato"}), 404
+
+@api.route('/update_cittadino', methods=['POST'])
+def update_cittadino():
+    content_type = request.headers.get('Content-Type')
+    if content_type == 'application/json':
+        jsonReq = request.json
+        codice_fiscale = jsonReq.get('codFiscale')
+        if codice_fiscale in cittadini:
+            cittadini[codice_fiscale] = jsonReq
+            return jsonify({"Esito": "200", "Msg": "Cittadino aggiornato con successo"}), 200
+        else:
+            return jsonify({"Esito": "200", "Msg": "Cittadino non trovato"}), 200
+    else:
+        return 'Content-Type non supportato!'
 
 @api.route('/elimina_cittadino', methods=['POST'])
 def elimina_cittadino():
     content_type = request.headers.get('Content-Type')
-    print("chiamata Ricevuta" + 'application_type')
-    if(content_type=='application/json'):
-        cod = request.json
-        jsonResp={"Esito":"200", "Msg": "errore cittadino non presente"}
+    if content_type == 'application/json':
+        cod = request.json.get('codFiscale')
         if cod in cittadini:
             del cittadini[cod]
             return jsonify({"Esito": "200", "Msg": "Cittadino rimosso con successo"}), 200
         else:
-            
-            return json.dumps(jsonResp)
-        
+            return jsonify({"Esito": "404", "Msg": "Cittadino non trovato"}), 404
+    else:
+        return 'Content-Type non supportato!'
 
 api.run(host="127.0.0.1", port=8080)
-
