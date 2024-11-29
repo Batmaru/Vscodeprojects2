3b98f1b74c3a0844b84eb28eaf23d9233b1b628b
Batmaru <marwanrafik02@gmail.com>
2024-11-29 13:43:10 +0100

    .

--- P20241125/server1.py
+++ P20241125/server1.py
@@ -1,4 +1,3 @@
-
 from flask import Flask, request, jsonify
 import json
 from dbclient import *
@@ -6,7 +5,7 @@ from datetime import datetime
 
 api = Flask(__name__)
 
-cur = connect()
+cur =connect()
 
 @api.route('/cerca_automobile', methods=['POST'])
 def cerca_automobile():
@@ -18,30 +17,26 @@ def cerca_automobile():
 
     query = """
     SELECT f.nome AS filiale, a.modello, a.marca, a.anno, a.disponibilita
-    FROM automobili a, filiali f
-    where f.id = a.filiale_id
-    and a.modello = '%s' AND a.marca = '%s';
-    """ % (modello, marca)  
+    FROM automobili a
+    JOIN filiali f ON f.id = a.filiale_id
+    WHERE a.modello = %s AND a.marca = %s;
+    """
 
     result = []
 
     try:
-        rows_count = read_in_db(cur, query)  
+        cur.execute(query, (modello, marca))
+        rows = cur.fetchall()
 
-        if rows_count > 0:
-            
-            for _ in range(rows_count):
-                status, row = read_next_row(cur)
-                if status == 0:  
-                    result.append({
-                        'filiale': row[0],
-                        'modello': row[1],
-                        'marca': row[2],
-                        'anno': row[3],
-                        'disponibilita': row[4]
-                    })
-                else:
-                    break  ì
+        if rows:
+            for row in rows:
+                result.append({
+                    'filiale': row[0],
+                    'modello': row[1],
+                    'marca': row[2],
+                    'anno': row[3],
+                    'disponibilita': row[4]
+                })
 
             return jsonify({"success": True, "automobili": result})
         else:
@@ -49,6 +44,8 @@ def cerca_automobile():
 
     except Exception as e:
         return jsonify({"success": False, "msg": f"Errore: {str(e)}"})
+
+
 
 if __name__ == '__main__':
     api.run(host="127.0.0.1", port=8080,ssl_context='adhoc')
